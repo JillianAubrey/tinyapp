d901209755b9d411a06f3c2111fbd8bf71a18ac9
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-10-04 15:06:53 +0000

    Added route to redirect hosrted urls to their long url

--- express_server.js
+++ express_server.js
@@ -37,6 +37,11 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+
 //POST
 app.post("/urls", (req, res) => {
   const id = generateRandomString(6);
